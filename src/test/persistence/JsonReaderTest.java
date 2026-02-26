81ed31c7100dfae523cbdcc7e95c8d9bd5ea8295
Tucker Drown <tdrown@students.cs.ubc.ca>
2026-02-26 13:38:51 -0800

    Added tests for the JsonReader class

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -1,8 +1,61 @@
 package persistence;
 
+import model.Resort;
+import model.Trail;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+
 import ca.ubc.cs.ExcludeFromJacocoGeneratedReport;
-
+// Code adapted from JsonSerializationDemo
 @ExcludeFromJacocoGeneratedReport
-public class JsonReaderTest {
+public class JsonReaderTest extends JsonTest{
 
+    @Test
+    public void testReaderNonExistentFile() {
+        JsonReader reader = new JsonReader("./data/noSuchFile.json");
+        try {
+            reader.read();
+            fail("IOException expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    public void testReaderEmptyResort() {
+        JsonReader reader = new JsonReader("./data/testReaderEmptyResort.json");
+        try {
+            Resort r = reader.read();
+            assertEquals("Empty resort", r.getName());
+            assertEquals("none", r.getRegion());
+            assertTrue(r.getTrails().isEmpty());
+        } catch (IOException e) {
+            fail("Could not read from file");
+        }
+    }
+    
+    @Test
+    public void testReaderGeneralResort() {
+        JsonReader reader = new JsonReader("./data/testReaderGeneralResort.json");
+        try {
+            Resort r = reader.read();
+            assertEquals("Resort", r.getName());
+            assertEquals("Region", r.getRegion());
+            List<Trail> trails = r.getTrails();
+            assertEquals(2, trails.size());
+            List<String> notes = new ArrayList<>();
+            List<Trail> downhill = new ArrayList<>();
+            notes.add("note");
+            downhill.add(r.getTrails().get(1));
+            checkTrail("name", "difficulty", "location", "features", notes, true, true, true, new ArrayList<Trail>(), trails.get(0));
+            checkTrail("name2", "difficulty2", "location2", "features2", new ArrayList<>(), false, false, false, downhill, trails.get(1));
+        }   catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
 }
--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -1,8 +1,23 @@
 package persistence;
 
+import model.Trail;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.List;
+
 import ca.ubc.cs.ExcludeFromJacocoGeneratedReport;
 
 @ExcludeFromJacocoGeneratedReport
 public class JsonTest {
-
+    protected void checkTrail(String name, String difficulty, String location, String features, List<String> notes, Boolean ridden, Boolean open, Boolean favorite, List<Trail> downhillTrails, Trail t) {
+        assertEquals(name, t.getName());
+        assertEquals(difficulty, t.getDifficulty());
+        assertEquals(location, t.getLocation());
+        assertEquals(features, t.getFeatures());
+        assertEquals(notes, t.getNotes());
+        assertEquals(ridden, t.isRidden());
+        assertEquals(open, t.isOpen());
+        assertEquals(favorite, t.isFavorite());
+        assertEquals(downhillTrails, t.getDownhillTrails());
+    }   
 }
